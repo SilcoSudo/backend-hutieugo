fc5b4ffa9a514f1a5be39040ec9de6760a08fa91
SilcoSudo <raymanvn@gmail.com>
2025-02-21 11:37:07 +0700

    d

---
+++ src/main/java/com/hutieugo/HutieugoApplication.java
@@ -0,0 +1,11 @@
+package com.hutieugo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HutieugoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HutieugoApplication.class, args);
+    }
+}

495b1a17b09842b4e169d49ac4453524ae5f3b4c
SilcoSudo <raymanvn@gmail.com>
2025-03-20 02:05:16 +0700

    /

--- src/main/java/com/hutieugo/backend/security/SecurityConfig.java
+++ src/main/java/com/hutieugo/backend/security/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.hutieugo.config;
+package com.hutieugo.backend.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
